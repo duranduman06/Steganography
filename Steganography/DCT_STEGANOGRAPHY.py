6b6f02a74dbd3a14f40745d575b1c46c81198061
Semicide <190101033@ogr.atu.edu.tr>
2023-12-11 21:55:42 +0300

    sonHalisi
    ses

--- Steganography/DCT_STEGANOGRAPHY.py
+++ Steganography/DCT_STEGANOGRAPHY.py
@@ -1,100 +1,109 @@
 from PIL import Image
-import os
-import cv2
 import numpy as np
-"""
-B. DCT Based Steganography  Algorithm to embed text message:- 
-    Step 1: Read cover image. 
-    Step 2: Read secret message and convert it in binary. 
-    Step 3: The cover image is broken into 8×8 block of pixels. 
-    Step 4: Working from left to right,  top  to bottom subtract 128 in each block of pixels. 
-    Step 5: DCT is applied to each block. 
-    Step 6: Each  block  is  compressed  through  quantization table. 
-    Step 7: Calculate LSB of each DC coefficient  and replace with each bit of secret message. 
-    Step 8: Write stego image. 
-
-Algorithm to retrieve text message:- 
-    Step 1: Read stego image. 
-    Step 2: Stego image is broken into 8×8 block of pixels. 
-    Step 3: Working from left to right,  top  to bottom subtract 128 in each block of pixels. 
-    Step 4: DCT is applied to each block. 
-    Step 5: Each  block  is  compressed  through  quantization table. 
-    Step 6: Calculate LSB of each DC coefficient. 
-    Step 7: Retrieve and convert each 8 bit into character.
-"""
-
-def openImage(imgPath):
-    cover_image = None  # Initialize img as None
-    try:
-        if os.path.exists(imgPath): #if the image path exists correctly
-            cover_image = cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE)
-        else:
-            print("Error: The specified path is incorrect. The file does not exist.")
-    except Exception as e:
-        print(f"Error: {e}")
-        print("An error occurred while trying to open the image.")
-
-    return cover_image  # Return the img variable
 
 
-def save_stego_image(image, filename):
-    try:
-        image.save(filename)
-        print(f"Stego image saved as {filename}")
-    except Exception as e:
-        print(f"Error: {e}")
-        print("An error occurred while trying to save the stego image.")
+def my_dct(block):
+    N = block.shape[0]
+    result = np.zeros_like(block, dtype=float)
+
+    for u in range(N):
+        cu = 1.0 / np.sqrt(2) if u == 0 else 1.0
+        for v in range(N):
+            cv = 1.0 / np.sqrt(2) if v == 0 else 1.0
+            sum_val = 0.0
+
+            for i in range(N):
+                for j in range(N):
+                    sum_val += block[i, j] * np.cos((2 * i + 1) * u * np.pi / (2 * N)) * np.cos(
+                        (2 * j + 1) * v * np.pi / (2 * N))
+
+            result[u, v] = cu * cv * sum_val / np.sqrt(N)
+
+    return result
 
 
+def my_idct(block):
+    N = block.shape[0]
+    result = np.zeros_like(block, dtype=float)
 
-def calculate_DCT(cover_image,txt):
-    # Converts text message into binary
-    binary_text = ''.join(format(ord(char), '08b') for char in txt)  # binary_text is a string
-    print("Binary representation of the text:", binary_text)
-    # Splits binary text into 8-bit segments and stores in a list
-    binary_list = [binary_text[i:i + 8] for i in range(0, len(binary_text), 8)]
-    print("Binary representation of the text as List:", binary_list)
+    for i in range(N):
+        for j in range(N):
+            sum_val = 0.0
 
+            for u in range(N):
+                for v in range(N):
+                    cu = 1.0 / np.sqrt(2) if u == 0 else 1.0
+                    cv = 1.0 / np.sqrt(2) if v == 0 else 1.0
+                    sum_val += cu * cv * block[u, v] * np.cos((2 * i + 1) * u * np.pi / (2 * N)) * np.cos(
+                        (2 * j + 1) * v * np.pi / (2 * N))
 
-# STEP-3 The cover image is broken into 8×8 block of pixels.
-    # Break the cover image into 8x8 blocks of pixels
-    block_size = 8
-    height, width = cover_image.shape
-    blocks = []
-    # Now, 'blocks' contains 8x8 pixel blocks of the cover image.
-    for y in range(0, height, block_size):
-        for x in range(0, width, block_size):
-            block = cover_image[y:y + block_size, x:x + block_size]
-            blocks.append(block)
-    print(blocks)
+            result[i, j] = sum_val / np.sqrt(N)
+
+    return result
 
 
+def dct_encode(image_path, message):
+    # Open the image
+    img = Image.open(image_path)
+
+    # Convert the image to grayscale
+    img = img.convert('L')
+
+    # Convert the image to a NumPy array
+    img_array = np.array(img)
+
+    # Embed the message in 8x8 blocks using the DCT
+    message_index = 0
+    for i in range(0, img_array.shape[0], 8):
+        for j in range(0, img_array.shape[1], 8):
+            block = img_array[i:i + 8, j:j + 8]
+            block_dct = np.round(my_dct(block))
+
+            # Embed the message in the DC coefficient of each block
+            if message_index < len(message):
+                block_dct[0, 0] = int(block_dct[0, 0]) & 0b11111110  # Clear the LSB
+                block_dct[0, 0] = int(block_dct[0, 0]) | (
+                    int(ord(message[message_index]) & 1))  # Set the LSB with the message bit
+
+                message_index += 1
+
+            img_array[i:i + 8, j:j + 8] = np.round(my_idct(block_dct))
+
+    # Convert the NumPy array back to an image
+    encoded_img = Image.fromarray(img_array.astype('uint8'))
+
+    # Save the encoded image
+    encoded_img.save('encoded_image.png')
+    print("Image encoded and saved as encoded_image.png")
 
 
-#def decode_DCT(image,key):
+def dct_decode(encoded_image_path, message_length):
+    # Open the encoded image
+    encoded_img = Image.open(encoded_image_path)
 
-def main():
-    imgPath = str(input("Please enter the path of the image: "))
-    img = openImage(imgPath)
-    if img is None:
-        print("Failed to open the image.")
-    else:
-        print("1: Encode")
-        print("2: Decode")
-        whoswho = str(input("Please Select a function (1 or 2): "))
-        if whoswho == '1':
-            txt = str(input("Please enter your text that you want to hide: ") + "D$T")
-            #encode_name = str(input("Please enter a file name (ex: image.png): "))
-            img = calculate_DCT(img,txt)
-            #save_image = save_stego_image(img, encode_name)  # Save the stego image
+    # Convert the image to a NumPy array
+    encoded_array = np.array(encoded_img)
 
-        elif whoswho == '2':
-            decoded_message = decode_DCT(img , "D$T")
-            print("Çözülen metin:", decoded_message)
-        else:
-            print("Please enter a valid message")
+    # Extract the message from the DC coefficient of each 8x8 block
+    message = ''
+    for i in range(0, encoded_array.shape[0], 8):
+        for j in range(0, encoded_array.shape[1], 8):
+            block = encoded_array[i:i + 8, j:j + 8]
+            dc_coefficient = block[0, 0]
+            message += str(int(dc_coefficient) & 1)
+
+    # Convert binary message to characters
+    decoded_message = ''.join([chr(int(message[i:i + 8], 2)) for i in range(0, len(message), 8)])
+
+    return decoded_message
 
 
+# Save the message in the image
+image_path = input("Enter the path of the image: ")
+message_to_hide = input("Enter the message to hide: ")
+dct_encode(image_path, message_to_hide)
 
-if __name__ == "__main__":
-    main()
+# Decode the message from the encoded image
+encoded_image_path = 'encoded_image.png'
+decoded_message = dct_decode(encoded_image_path, len(message_to_hide))
+print("Decoded Message:", decoded_message)
